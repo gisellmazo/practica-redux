964bd9b4243daa5b648a54a77ef565f8b0e3cc17
Santiago Betancur Graciano <santiago.betancur@sura-am.com>
2021-06-04 16:14:35 -0500

    Pulled de Gisell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent]
